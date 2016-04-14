c030a2dbb8746de500ac22e387e92b2dd4c277d1
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-04-14 14:18:41 +0530

    bootstrap + font-awesome + jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,13 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link href="{{ url('/assets/font-awesome/css/font-awesome.min.css') }}" rel='stylesheet' type='text/css'>
+    <!--<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>-->
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ url('/assets/bootstrap/css/bootstrap-theme.min.css') }}" rel='stylesheet' type='text/css'>
+	<link href="{{ url('/assets/bootstrap/css/bootstrap.min.css') }}" rel='stylesheet' type='text/css'>
+	
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -75,8 +77,8 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="{{ url('/assets/jquery-2.2.3.min.js') }}"></script>
+	<script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
